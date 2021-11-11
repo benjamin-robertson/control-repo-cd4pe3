af5017127dcafa2445b369fc6c4d97a00eb83b61
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-11 16:05:28 +1100

    Udpates!!! added user

--- site-modules/profile/manifests/windows/users.pp
+++ site-modules/profile/manifests/windows/users.pp
@@ -1,9 +1,15 @@
 class profile::windows::users {
 
-  # TODO: Alignment
+  # TODO: Alignmentddf
   user { 'ben':
     ensure  => 'present',
     comment => 'bens user account lol',
+    groups  => 'Administrators'
+  }
+
+  user { 'ben123':
+    ensure  => 'present',
+    comment => 'bens user account lol123',
     groups  => 'Administrators'
   }
 
